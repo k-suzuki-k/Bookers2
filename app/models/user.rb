9cb120b07bd5bce108ea86f72c52b0868a0579c8
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-10-15 16:21:02 +0900

    [Add] sign_in sign_up

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  validates :name,uniqueness: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def email_required?
+	  false
+  end
+  def email_changed?
+	  false
+  end
+
+end
